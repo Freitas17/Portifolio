cb9ce593f80b7f5293701e171e3416f83438193a
André De Freitas <ndrfreita@gmail.com>
2025-10-06 02:51:27 -0300

    Consolida rotas no app.py para corrigir deploy

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
-from views import main_routes
-
-# Cria a instância da aplicação Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-app.register_blueprint(main_routes)
+# 2. Definição das Rotas do Site
+# Cada função corresponde a uma página do seu site.
 
-# Permite executar o arquivo diretamente com 'python app.py'
+@app.route('/')
+def index():
+    # Esta rota vai renderizar o seu index.html (página "Sobre")
+    return render_template('index.html')
+
+@app.route('/projetos')
+def projetos():
+    # Esta rota vai renderizar o seu projetos.html
+    return render_template('projetos.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
